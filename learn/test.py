ddd01c41947de54f42e527ba3969c3c04de6e7fa
David Lin <linshan@gmail.com>
2020-03-12 16:06:03 +0800

    learn python

--- learn/test.py
+++ learn/test.py
@@ -1,5 +1,16 @@
-#%%
-print ("Hello, world")
+# %%
+
+names = ['xiayu', 'pengle', 'qindi', 'yangjun']
+
+message = f"Hello,  {names[0].title()}!"
+print(message)
+message = f"Hello,  {names[1].title()}!"
+print(message)
+message = f"Hello,  {names[2].title()}!"
+print(message)
+message = f"Hello,  {names[3].title()}!"
+print(message)
+
 
 
 # %%
